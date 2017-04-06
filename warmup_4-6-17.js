24dd2f3dd5b82ccfd927c13cb13f35591e99d5a0
ellebayless <elle@ellebayles.com>
2017-04-06 09:19:21 -0700

    reformat 4-6-17 warmup

--- warmup_4-6-17.js
+++ warmup_4-6-17.js
@@ -1,4 +1,5 @@
 //Write out the skeleton of a RESTful Express server.
+
 //CONFIGURATION
 var express = require('express');
 var file = require('./file');
@@ -8,42 +9,28 @@ var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
+
 //ROUTES
 //get all unicorns
 app.get('/file', function (req, res) {
-  res.send(unicorns);
 });
 
 //get specific unicorn
 app.get('/file/:id', function (req, res) {
-  res.send(unicorns[req.params.id]);
 });
 
 //create new unicorn
 app.post('/file', function (req, res) {
-  unicorns.push(req.body);
-  res.send(unicorns);
 });
 
 //change info in unicorn obj
 app.put('/file/:id', function (req, res) {
-  var unicornLocation = unicorns.filter(function (num) {
-    return num.id === parseInt(req.params.id);
-  });
-  var specialUnicorn = unicorns.indexOf(unicornLocation[0]);
-  unicorns[specialUnicorn] = req.body;
-  res.send(unicorns);
 });
 
 //delete unicorn :(
 app.delete('/file/:id', function (req, res) {
-  var unicornLocation = unicorns.filter(function (num) {
-    return num.id === parseInt(req.params.id);
-  });
-  var unlovedUnicorn = unicorns.indexOf(unicornLocation[0]);
-  unicorns.splice(unlovedUnicorn, 1);
-  res.send(unicorns);
 });
+
 
 //LISTENER
 app.listen(port, function () {
