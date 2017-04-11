c84db4b31c7dde2488f719a30bd8b8d62ba72d52
ellebayless <elle@ellebayles.com>
2017-04-11 10:55:44 -0700

    commit 4-11-17 warmup

--- warmup_4-11-17.js
+++ warmup_4-11-17.js
@@ -1,16 +1,25 @@
+//PART ONE
 // Write a formula that calculates the distance between two points on a cartesian coordinate system. The formula should accept an array of two points.
 
 function distance (arr1, arr2) {
-  var x1 = arr1[0];
-  var y1 = arr1[1];
-  var x2 = arr2[0];
-  var y2 = arr2[1];
-
-  var xPointsSquared = ((x2-x1) * (x2-x1));
-  var yPointsSquared = ((y2-y1) * (y2-y1));
-  var xPointsPlusYPoints = xPointsSquared + yPointsSquared;
-
-  return Math.sqrt(xPointsPlusYPoints);
+  var xPointsSquared = ((arr2[0] - arr1[0]) ** 2);
+  var yPointsSquared = ((arr2[1] - arr1[1]) ** 2);
+  return Math.sqrt(xPointsSquared + yPointsSquared);
 }
-
 distance([2,4], [4,6]);
+
+
+//PART TWO
+// Spin up an Express app that has a route that does the following:
+// When you visit http://localhost:3000/calculate/5/3
+// It should do a res.send showing '15' on the screen.
+
+var express = require('express');
+var router = express.router();
+
+router.get('/calculate/:foo/:bar', function (req, res) {
+  var stuff = res.params.foo * res.params.bar;
+  res.send('This is your calcutaion: ' + stuff);
+});
+
+app.listen(blahblah)
